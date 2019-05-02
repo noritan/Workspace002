687f2c44c436eacd71eeb1b54e38f864fa248968
Nori Tan <github@noritan.org>
2019-05-02 14:41:57 +0900

    Added a project TPH_Dump
    Create a PTH_Dump project to dump compensated TPH data to UART

---
+++ TPH_Dump.cydsn/main.c
@@ -0,0 +1,29 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+#include "project.h"
+
+int main(void)
+{
+    CyGlobalIntEnable; /* Enable global interrupts. */
+
+    /* Place your initialization/startup code here (e.g. MyInst_Start()) */
+    UART_Start();
+    UART_UartPutString("HELLO WORLD\r\n");
+    I2CM_Start();
+
+    for(;;)
+    {
+        /* Place your application code here. */
+    }
+}
+
+/* [] END OF FILE */
